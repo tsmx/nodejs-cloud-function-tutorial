144738fef0d485465d6d77087047ae718ffa96a2
tsmx <dev@tsmx.net>
2023-08-29 22:25:28 +0200

    Function code changed

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const config = secureConfig();
 export const helloGCP = (req, res) => {
     res.json({
         info: 'Hello from GCP cloud functions!',
-        nodeEnv: process.env.NODE_ENV,
         secret: config.secret
     });
 }
